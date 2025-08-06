d48111e71c1fef9cdc9e2441eb8dcc304bd33a64
DeeDz <deed_93@web.de>
2025-08-06 15:34:18 +0200

    added dependencies for automated test

--- server.test.js
+++ server.test.js
@@ -2,11 +2,13 @@
 const request = require('supertest');
 const express = require('express');
 const app = express();
+const text = 'Hallo Welt';
 
 app.use(express.json());
 app.post('/api/text', (req, res) => res.json({ reply: `Empfangen: ${req.body.text}` }));
 
 test('POST /api/text', async () => {
-  const res = await request(app).post('/api/text').send({ text: 'Hallo' });
-  expect(res.body.reply).toBe('Empfangen: Hallo');
+  const res = await request(app).post('/api/text').send({ text });
+  expect(res.body.reply).toBe(`Empfangen: ${text}`);
+  expect(res.statusCode).toBe(200);
 });
